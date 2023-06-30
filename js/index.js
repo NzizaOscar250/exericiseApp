4f6041d6dc26522d8ec54b7fd1bc041eb2b1bf1f
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-06-29 22:58:27 -0700

    axios added but not used, probably in the future

--- js/index.js
+++ js/index.js
@@ -1,21 +1,35 @@
-const axios = require('axios/dist/browser/axios.cjs');
-
+const url = 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList';
 const options = {
-  method: 'GET',
-  url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
-  headers: {
-    'X-RapidAPI-Key': 'ff96a67380msh75f2615b7e8a290p1b32eejsnd5fd814ae9b6',
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-  }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ff96a67380msh75f2615b7e8a290p1b32eejsnd5fd814ae9b6',
+		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+	}
 };
 
-const Home=async()=>{
+
+//normal settings
+
+
+ const mobileNavigationView=document.querySelector("#mobile-menu");
+const searchContainer=document.querySelector("#search")
+ // const btn=document.querySelector("#mobile-button");
+
+function mobileView(){
+    mobileNavigationView.classList.toggle("hidden");
+}
+function Search(){
+    searchContainer.classList.toggle("hidden");
+}
+
+async function getData(){
     try {
-        const response = await axios.request(options);
-        console.log(response.data);
+        const response = await fetch(url, options);
+        const result = await response;
+        console.log(result);
     } catch (error) {
         console.error(error);
     }
 }
 
-Home();
+getData()
